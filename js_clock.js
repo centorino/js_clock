750377e22a6b0eb6437b7e5253344b9c86c9e382
centorino <lazyconsole.com@gmail.com>
2015-03-19 15:17:02 +0900

    Update js_clock.js
    added "ampm" and "ampmHours" to return json object.
    added start and stop methods as instance method to JsClock class.

--- js_clock.js
+++ js_clock.js
@@ -4,29 +4,50 @@
   JsClock = function() {
     function JsClock(options) {
       this.wday = ['日', '月', '火', '水', '木', '金', '土'];
-      this.now  = options.startDate || new Date();
+      this.now  = options.startDate || new Date();      
+      this.callback = options.callback || undefined;
 
       this.debug = options.debug || false;
+      
+      this.intervalId = null;
 
       var self = this;
 
+      if (options.autodo === undefined || !options.autodo) return;
+      
       window.onload = function() {
-        var elem = document.getElementById('debugClock');
-
-        setInterval(function() {
-          self.now.setSeconds(self.now.getSeconds() + 1);
-
-          if (self.debug && elem !== undefined) elem.innerHTML = self._debugFormatDate();
-          if (options.callback !== undefined) options.callback(self._getNowJson());
-        }, 1000);
+        self.start();
       };
     }
+    
+    JsClock.prototype.start = function() {
+      var elem = document.getElementById('debugClock');
+      var self = this;
+      
+      self.intervalId = setInterval(function() {
+        self.now.setSeconds(self.now.getSeconds() + 1);
+
+        if (self.debug && elem !== undefined) elem.innerHTML = self._debugFormatDate();
+        if (self.callback !== undefined) self.callback(self._getNowJson());
+      }, 1000);
+    };
+    JsClock.prototype.stop = function() {
+      if (this.intervalId !== null) {
+        clearInterval(this.intervalId);
+      }
+    };
 
     JsClock.prototype._paddingZero = function(s, l) {
       if (s.length < l) for (var i = 0; i < l - s.length; i++) s = '0' + s;
       return s;
     };
     JsClock.prototype._getNowJson = function() {
+      var ampm = 'am';
+      var ampmHours = this.now.getHours();
+      if (this.now.getHours() > 12) {
+        ampm = 'pm';
+        ampmHours -= 12;
+      }
       return { year: this.now.getFullYear().toString(),
                month: this._paddingZero((this.now.getMonth() + 1).toString(), 2),
                date: this._paddingZero(this.now.getDate().toString(), 2),
@@ -34,7 +55,10 @@
                times: this.now.toLocaleString().split(' ')[1],
                hours: this._paddingZero(this.now.getHours().toString(), 2),
                minutes: this._paddingZero(this.now.getMinutes().toString(), 2),
-               seconds: this._paddingZero(this.now.getSeconds().toString(), 2) };
+               seconds: this._paddingZero(this.now.getSeconds().toString(), 2),
+               ampm: ampm,
+               ampmHours: this._paddingZero(ampmHours.toString(), 2)
+             };
     };
     JsClock.prototype._debugFormatDate = function() {
       var i = this._getNowJson();
